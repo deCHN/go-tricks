751323de460daacf2edd7ea85c4188fce9dd997a
deChn <hey.ceth@gmail.com>
2020-04-16 22:25:14 +0200

    Add slice arg test.

--- slice_test.go
+++ slice_test.go
@@ -5,12 +5,27 @@ import (
 	"testing"
 )
 
-func TestSliceArg(t *testing.T) {
+func TestAppendSelf(t *testing.T) {
 	double := func(s []int) {
 		s = append(s, s...)
+		//s = append(s, s[0], s[1])
 	}
 
 	s := []int{1, 2, 3}
 	double(s)
 	fmt.Println(s, len(s)) //prints [1,2,3] 3
+}
+
+func TestArg(t *testing.T) {
+	addOne := func(s []int) []int {
+		for k, v := range s {
+			s[k] = v + 1
+		}
+		return s
+	}
+
+	s := []int{1, 2, 3}
+	r := addOne(s)
+	fmt.Println("origin:", s)   //[2,3,4]
+	fmt.Println("returned:", r) //[2,3,4]
 }
