38f9580b3b4dc57d46f92fa79b92250d15e5f3e9
deChn <hey.ceth@gmail.com>
2020-06-06 23:02:44 +0200

    Add closure example.

---
+++ closures_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestClosures(t *testing.T) {
+	for i := 10.0; i < 100; i += 10.0 {
+		rad := func() float64 {
+			return i * 39.370
+		}()
+		fmt.Printf("%.2f Meter = %.2f Inch\n", i, rad)
+	}
+}
